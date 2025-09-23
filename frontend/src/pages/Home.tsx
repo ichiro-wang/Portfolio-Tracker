bdd0bff95ac825bdf1eedd1764bd63332c910186
Ichiro Wang <ichiwang27@gmail.com>
2025-09-23 14:40:45 -0700

    home page message

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,21 +12,32 @@ const Home = () => {
   }
 
   return (
-    <Box className="w-[30rem] items-center justify-between">
-      <div>
-        <RoundedImage src="/default.webp" alt="Profile Pic" />
-        <h1 className="text-center">{user.name}</h1>
-      </div>
-      <div>
-        <h1 className="text-end">Totals</h1>
-        <p className="text-end">
-          Book Value: {formatCurrency(user.bookValue)}
-        </p>
-        <p className="text-end">
-          Market Value: {formatCurrency(user.marketValue)}
-        </p>
-      </div>
-    </Box>
+    <div className="flex w-[30rem] flex-col gap-3">
+      <Box className="items-center justify-between">
+        <div>
+          <RoundedImage src="/default.webp" alt="Profile Pic" />
+          <h1 className="text-center">{user.name}</h1>
+        </div>
+        <div>
+          <h1 className="text-end">Totals</h1>
+          <p className="text-end">
+            Book Value: {formatCurrency(user.bookValue)}
+          </p>
+          <p className="text-end">
+            Market Value: {formatCurrency(user.marketValue)}
+          </p>
+        </div>
+      </Box>
+      <Box>
+        <ul className="font-semibold">
+          <li className="">The free stock api I am using has a limit.</li>
+          <li>
+            Market prices may not be available or may not be reflective of the
+            most current values.
+          </li>
+        </ul>
+      </Box>
+    </div>
   );
 };
 
