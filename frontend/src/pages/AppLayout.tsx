e614923634237ed820045afda006248eb2e34820
Ichiro Wang <ichiwang27@gmail.com>
2025-11-11 13:03:21 -0800

    loading problem

--- frontend/src/pages/AppLayout.tsx
+++ frontend/src/pages/AppLayout.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useUser } from "../features/authentication/useUser";
+import FullPage from "../components/FullPage";
+import Loader from "../components/Loader";
 
 const AppLayout = () => {
+  const{ isLoading }=useUser()
+  
+  if (isLoading) {
+    return <FullPage><Loader /></FullPage>
+  }
+
   return (
     <>
       <Navbar />
